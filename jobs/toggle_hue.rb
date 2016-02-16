2fdf0c32806b135fc75c5a75f251d66104c38d5a
Hampus Nordin <nordin.hampus@gmail.com>
2016-02-16 08:02:09 +0100

    Added more error handling to hue job and widget

--- jobs/toggle_hue.rb
+++ jobs/toggle_hue.rb
@@ -1,13 +1,47 @@
 require 'hue'
 require 'json'
 
+def new_hue_client
+  client = nil
+  err_msg = ''
+  # Setup i18n
+  I18n.default_locale = settings.default_locale
+  I18n.locale = settings.locale
+  
+  begin
+    client = Hue::Client.new
+  rescue Hue::NoBridgeFound => e
+    err_msg = I18n.t('jobs.toggle_hue.errors.no_bridge_found')
+  rescue StandardError => e
+    err_msg = e.message
+  end
+
+  unless err_msg.empty?
+    send_event('toggle_hue', {
+      error: err_msg
+    });  
+  end
+
+  client
+end
+
 def group_is_on?
-  client = Hue::Client.new
-  group_is_on = true
-  client.lights.each { |light|
-    group_is_on = group_is_on && light.on?
-  }
-  group_is_on
+  client = new_hue_client
+  unless client.nil?
+    group_is_on = true
+    client.lights.each { |light|
+      group_is_on = group_is_on && light.on?
+    }
+    group_is_on
+  else
+    false
+  end
+end
+
+get '/hue/connectivity' do
+  connectivity = !new_hue_client.nil?
+
+  {:status => "success", :connectivity => connectivity}.to_json
 end
 
 get '/hue/state' do
@@ -28,15 +62,17 @@ end
 post '/hue/state' do
   content_type :json
 
-  client = Hue::Client.new
-  if params[:new_state] == 'on'
-    client.lights.each { |light|
-      light.on!
-    }
-  else
-    client.lights.each { |light|
-      light.off!
-    }
+  client = new_hue_client
+  unless client.nil?
+    if params[:new_state] == 'on'
+      client.lights.each { |light|
+        light.on!
+      }
+    else
+      client.lights.each { |light|
+        light.off!
+      }
+    end
   end
 
   {:status => "success"}.to_json
--- widgets/toggle_hue/toggle_hue.coffee
+++ widgets/toggle_hue/toggle_hue.coffee
@@ -7,6 +7,17 @@ class Dashing.ToggleHue extends Dashing.ClickableWidget
     get: -> @_state ? 'off'
     set: (key, value) -> @_state = value
 
+  @accessor 'connectivity',
+    get: -> @_connectivity ? false
+    set: (key, value) -> @_connectivity = value
+
+  checkConnectivity: ->
+    $.get '/hue/connectivity',
+      (data) =>
+        if data.status == 'success'
+          @set 'connectivity', data.connectivity
+          $(@node).toggleClass('no-connectivity', !data.connectivity)
+
   updateState: ->
     $.get '/hue/state',
       (data) =>
@@ -17,14 +28,20 @@ class Dashing.ToggleHue extends Dashing.ClickableWidget
     $.post '/hue/state',
       new_state: newState,
       (data) =>
-        @updateState();
+        @updateState()
 
   ready: ->
     # This is fired when the widget is done being rendered
+    @checkConnectivity()
 
   onData: (data) ->
-    @set 'state', data.state
+    if data.state
+      @set 'state', data.state
+
+    if data.error
+      @set 'status', data.error
+      $(@node).addClass('no-connectivity')
 
   onTap: (event) ->
     newState = if @get('state') == 'on' then 'off' else 'on'
-    @setState(newState);
+    @setState(newState)
