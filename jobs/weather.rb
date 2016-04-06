affcd1fb41b779ebc9f9c9114939a04a5e65bdf0
Hampus Nordin <nordin.hampus@gmail.com>
2016-04-06 20:20:48 +0200

    Ditched Yahoo Weather since it stopped working due to api changes. Using SMHI instead.

--- jobs/weather.rb
+++ jobs/weather.rb
@@ -1,130 +1,94 @@
 require 'net/http'
-require 'xmlsimple'
+require 'uri'
+require 'json'
+require 'time'
+
+weather_uri = URI(ENV['WEATHER_URI'] || '')
+
  
-# Get a WOEID (Where On Earth ID)
-# for your location from here:
-# http://woeid.rosselliot.co.nz/
-woe_id = ENV['WEATHER_WIDGET_WOE_ID'] || 894423
- 
-# Temerature format:
-# 'c' for Celcius
-# 'f' for Fahrenheit
-format = ENV['WEATHER_WIDGET_FORMAT'] || 'c'
- 
-SCHEDULER.every '1s', :first_in => 0 do |job|
-  begin  
-    http = Net::HTTP.new('weather.yahooapis.com')
-    response = http.request(Net::HTTP::Get.new("/forecastrss?w=#{woe_id}&u=#{format}"))
-    weather_data = XmlSimple.xml_in(response.body, { 'ForceArray' => false })['channel']['item']['condition']
-    weather_location = XmlSimple.xml_in(response.body, { 'ForceArray' => false })['channel']['location']
-    send_event('weather', { :temp => "#{weather_data['temp']}&deg;#{format.upcase}",
-                            :condition => weather_data['text'],
-                            :title => "#{weather_location['city']}",
-                            :climacon => climacon_class(weather_data['code']),
-                            :error => ""})
-  rescue StandardError => e
-    send_event('weather', {error: e.message})
+SCHEDULER.every '10m', :first_in => 0 do |job|
+  # Setup i18n
+  I18n.default_locale = settings.default_locale
+  I18n.locale = settings.locale
+
+  # Create request
+  req = Net::HTTP::Get.new(weather_uri)
+
+  # Send request
+  res = Net::HTTP.start(weather_uri.hostname, weather_uri.port) { |http|
+    http.request(req)
+  }
+
+  if res && res.body
+    weather_data = JSON.parse(res.body)
+
+    timeseries = weather_data['timeseries']
+
+    closest = find_item_closest_to_date(timeseries, Time.now.utc.iso8601)
+
+    send_event('weather', {
+      now: weather_hash(closest)
+    })
+  end
+end
+
+def weather_hash(item)
+  {
+    temp: "#{item['t']}&deg;C",
+    condition: weather_category(item['pcat']),
+    title: "Jönköping",
+    climacon: climacon_class(item['pcat']),
+    error: ""  
+  }
+end
+
+
+def weather_category(pcat)
+  case pcat.to_i
+  when 0
+    'Okänt väder' # no
+  when 1
+    'Snö' # snow
+  when 2
+    'Snöblandat regn' # snow and rain
+  when 3
+    'Regn' # rain
+  when 4
+    'Duggregn' # drizzle
+  when 5
+    'Isande regn' # freezing rain
+  when 6
+    'Isande duggregn' # freezing drizzle
   end
 end
 
 
-def climacon_class(weather_code)
-  case weather_code.to_i
-  when 0 
-    'tornado'
-  when 1 
-    'tornado'
-  when 2 
-    'tornado'
-  when 3 
-    'lightning'
-  when 4 
-    'lightning'
-  when 5 
-    'snow'
-  when 6 
-    'sleet'
-  when 7 
-    'snow'
-  when 8 
-    'drizzle'
-  when 9 
-    'drizzle'
-  when 10 
-    'sleet'
-  when 11 
-    'rain'
-  when 12 
-    'rain'
-  when 13 
-    'snow'
-  when 14 
-    'snow'
-  when 15 
-    'snow'
-  when 16 
-    'snow'
-  when 17 
-    'hail'
-  when 18 
-    'sleet'
-  when 19 
-    'haze'
-  when 20 
-    'fog'
-  when 21 
-    'haze'
-  when 22 
-    'haze'
-  when 23 
-    'wind'
-  when 24 
-    'wind'
-  when 25 
-    'thermometer low'
-  when 26 
-    'cloud'
-  when 27 
-    'cloud moon'
-  when 28 
-    'cloud sun'
-  when 29 
-    'cloud moon'
-  when 30 
-    'cloud sun'
-  when 31 
-    'moon'
-  when 32 
-    'sun'
-  when 33 
-    'moon'
-  when 34 
-    'sun'
-  when 35 
-    'hail'
-  when 36 
-    'thermometer full'
-  when 37 
-    'lightning'
-  when 38 
-    'lightning'
-  when 39 
-    'lightning'
-  when 40 
-    'rain'
-  when 41 
-    'snow'
-  when 42 
-    'snow'
-  when 43 
-    'snow'
-  when 44 
-    'cloud'
-  when 45 
-    'lightning'
-  when 46 
-    'snow'
-  when 47 
-    'lightning'
+def find_item_closest_to_date(items, findDate)
+  unless findDate.instance_of? Time
+    findDate = Time.parse(findDate)
+  end
+
+  items.sort_by { |date|
+    (Time.parse(date['validTime']) - findDate).abs
+  }.first
+end
+
+
+def climacon_class(pcat)
+  case pcat.to_i
+  when 0
+    'cloud' # no
+  when 1
+    'snow' # snow
+  when 2
+    'rain' # snow and rain
+  when 3
+    'rain' # rain
+  when 4
+    'drizzle' # drizzle
+  when 5
+    'rain' # freezing rain
+  when 6
+    'drizzle' # freezing drizzle
   end
 end
--- widgets/weather/weather.coffee
+++ widgets/weather/weather.coffee
@@ -4,6 +4,6 @@ class Dashing.Weather extends Dashing.I18nWidget
     # This is fired when the widget is done being rendered
 
   onData: (data) ->
-    if data.climacon
+    if data?.now?.climacon
       # reset classes
-      $('i.climacon').attr 'class', "climacon icon-background #{data.climacon}"
+      $('i.climacon').attr 'class', "climacon icon-background #{data.now.climacon}"
